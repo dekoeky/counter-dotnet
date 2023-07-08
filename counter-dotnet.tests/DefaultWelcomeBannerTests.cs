1619d93349887d60466eb78f603604e9f2f9986c
Wesley Van Koeckhoven <wvk@outlook.be>
2023-07-08 13:20:48 +0200

    Add new feature

---
+++ counter-dotnet.tests/DefaultWelcomeBannerTests.cs
@@ -0,0 +1,23 @@
+﻿namespace counter_dotnet.tests;
+
+/// <summary>
+/// <see cref="DefaultWelcomeBannerGenerator"/> related tests.
+/// </summary>
+[TestClass]
+public class DefaultWelcomeBannerTests
+{
+    [TestMethod]
+    public void TestConstructor()
+    {
+        //Arrange
+        var banner = new DefaultWelcomeBannerGenerator();
+
+        //Act
+        var result = banner.GetBanner();
+
+        //Assert
+        Assert.IsNotNull(result);
+        Assert.IsFalse(string.IsNullOrEmpty(result));
+        Assert.IsFalse(string.IsNullOrWhiteSpace(result));
+    }
+}
